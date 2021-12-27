d76d1c8b2fb8f91013c88f9bf061d4131ba82162
GlebCoder <gberliand@gmail.com>
2021-12-27 22:33:47 +0300

    first attempt

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, size(), null);
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+
+        Arrays.sort(storage, new Comparator<Resume>() {
+            @Override
+            public int compare(Resume resume, Resume t1) {
+                if (resume == null && t1 == null) {
+                    return 0;
+                }
+                if(resume == null) {
+                    return 1;
+                }
+                if(t1 == null) {
+                    return -1;
+                }
+                return resume.uuid.compareTo(t1.uuid);
+            }
+        });
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
+// Determines amount of Resumes in storage
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+        return storage.length;
     }
+
 }
